e7bba515e44e7c2fe7b577b1603aebec40f7a9eb
Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>
2026-06-04 08:40:16 -0300

    Initial commit
    Signed-off-by: Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>

---
+++ bot.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+TMP_DIR=$(mktemp -d)
+PRODUCTCOMPOSE_FILE="000productcompose/default.productcompose"
+IBS_BUILD_PROJECT="SUSE:SLFO:Main"
+
+trap 'rm -rf "${TMP_DIR}"' EXIT
+
+debug() {
+    printf -- "-- DEBUG START --\n%b\n" "$*" >&2
+    printf -- "-- DEBUG END --\n" >&2
+}
+
+# clone SLES repo if not inside a git repository or productcompose file does not exist
+if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1 || [ ! -f "${PRODUCTCOMPOSE_FILE}" ]; then
+    cd "${TMP_DIR}"
+    git clone gitea@src.suse.de:products/SLES.git
+    pushd "${TMP_DIR}/SLES"
+fi
+
+# retrieve the lastet binaries added
+GIT_DIFF=$(git show $(git log -1 --format="%H" -- ${PRODUCTCOMPOSE_FILE}) -- ${PRODUCTCOMPOSE_FILE})
+BINARIES=$(awk '/^\+ / && !/#-/ { $1=$1; print $3 }' <<< "${GIT_DIFF}" | sort -u)
+# START: override for test purpose
+BINARIES="${BINARIES}
+cargo1.94
+cargo1.81
+graphviz
+graphviz-devel
+graphviz-plugins-core
+foobar"
+# END: override for test purpose
+debug "NEW BINARIES:\\n${BINARIES}"
+
+# find the source packages
+RESULTS=$(while read -r binary; do
+    [ -z "$binary" ] && continue
+    source_pkg=$(osc -A https://api.suse.de bse -B "${IBS_BUILD_PROJECT}" --csv "${binary}" | awk -F'|' -v project="${IBS_BUILD_PROJECT}" '$1 == project { print $2 }')
+    
+    if [ -n "$source_pkg" ]; then
+        echo "SRC:${source_pkg}"
+    else
+        echo "FAIL:${binary}"
+    fi
+done <<< "${BINARIES}")
+
+# Extract variables from results
+SOURCES=$(awk -F':' '/^SRC:/ { print $2 }' <<< "${RESULTS}" | sort -u)
+FAILED_BINARIES=$(awk -F':' '/^FAIL:/ { print $2 }' <<< "${RESULTS}" | sort -u)
+# START: override for test purpose
+SOURCES="${SOURCES}
+kernel-default
+patterns-containers
+patterns-container"
+# END: override for test purpose
+printf -- "-- NEW SOURCES:\n%s\n" "${SOURCES}"
+debug "FAILED BINARIES (No source found):\n${FAILED_BINARIES}"
+
+# retrieve maintainership json
+MAINTAINERSHIP_JSON=$(git archive --remote=ssh://gitea@src.suse.de/products/SLFO.git slfo-main _maintainership.json | tar -xO)
+
+# check if new sources has a maintainer in maintainership json
+jq -rn '
+    input as $db |
+    $ARGS.positional[] as $pkg |
+    if ($db.packages[$pkg] | . != null and ((.users | . != null and length > 0) or (.groups | . != null and length > 0))) then
+        empty
+    else
+        "-- ORPHANED: \($pkg)"
+    end
+' --args ${SOURCES} <<< "${MAINTAINERSHIP_JSON}"
